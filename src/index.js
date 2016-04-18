2c0ad5fadff2fd17eda7d61cc23c26a1f70c495e
EvgSem <esemindieieva@gmail.com>
2016-04-19 00:35:51 +0200

    crud recipe name

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.get('/recipes', function (req, res) {
 });
 
 app.post('/addRecipe', function (req, res) {
-    let Recipe = mongoose.model('BeefRecipe', recipeSchema);
+    let Recipe = mongoose.model('Recipe', recipeSchema);
     let newRecipe = new Recipe({name: req.body.name, ingredients: []});
     newRecipe.save(function (err) {
         if (err) return console.error(err);
@@ -32,6 +32,49 @@ app.post('/addRecipe', function (req, res) {
     res.status(200);
 });
 
+app.post('/editRecipe', function (req, res) {
+    let recipeId = req.param('id');
+    let recipeName = req.param('name');
+    let promise = new Promise(function(resolve) {
+        recipeModel.findById(recipeId, function (err, recipe) {
+            recipe.name = recipeName;
+            recipe.save(function (err) {
+                if (err) return console.error(err);
+            });
+            resolve(recipe);
+        });
+    });
+    promise.then(function(result){
+        res.send(result);
+    });
+});
+
+app.get('/deleteRecipe', function (req, res) {
+    let recipeId = req.param('id');
+    let recipeName='';
+    let promise = new Promise(function(resolve) {
+        recipeModel.findById(recipeId, function (err, recipe) {
+            recipeName = recipe.name;
+            recipe.remove();
+            resolve("Recipe "+ recipeName +" was deleted");
+        });
+    });
+    promise.then(function(result){
+        res.send(result);
+    });
+});
+
+app.get('/recipe', function(req, res) {
+    let recipeId = req.param('id');
+    let promise = new Promise(function(resolve) {
+        recipeModel.find({'_id': recipeId}, function (err, docs) {
+            resolve(docs[0]);
+        });
+    });
+    promise.then(function(result){
+        res.send(result);
+    });
+});
 
 createConnection(function(){
     app.listen(3000, function () {
